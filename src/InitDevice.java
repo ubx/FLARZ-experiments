2689d73abe2063c6252ff756903dbc1b6e5b8945
Andreas Luethi <andreas.luethi@gmx.net>
2014-06-27 15:48:46 +0200

    More experiments...

--- src/InitDevice.java
+++ src/InitDevice.java
@@ -1,4 +1,6 @@
 import jssc.SerialPort;
+import jssc.SerialPortEvent;
+import jssc.SerialPortEventListener;
 import jssc.SerialPortException;
 
 /**
@@ -6,24 +8,63 @@ import jssc.SerialPortException;
  */
 public class InitDevice {
 
+    static SerialPort serialPort = new SerialPort("COM19");
 
     public static void main(String[] args) {
-        SerialPort serialPort = new SerialPort("COM19");
         try {
+
             System.out.println("Port opened: " + serialPort.openPort());
             System.out.println("Params setted: " + serialPort.setParams(19200, 8, 1, 0));
-            for (int i = 0; i < 20; i++) {
-                System.out.println("\"Hello World!!!\" successfully writen to port: " + serialPort.writeBytes(("Hello World!!! " + i).getBytes()));
+            serialPort.setEventsMask(SerialPort.MASK_RXCHAR + SerialPort.MASK_CTS + SerialPort.MASK_DSR);//Set mask
+            serialPort.addEventListener(new SerialPortReader());//Add SerialPortEventListener
+            System.out.println("\"ER_CMD#C?\" successfully writen to port: " + serialPort.writeBytes(("ER_CMD#C?").getBytes()));
+
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException ex) {
+                Thread.currentThread().interrupt();
+            }
+            for (int i = 0; i < 0; i++) {
+                System.out.println("\"Hello World!!!\" successfully writen to port: " + serialPort.writeBytes(("Hello World!!! ").getBytes()));
                 try {
                     Thread.sleep(1000);
                 } catch (InterruptedException ex) {
                     Thread.currentThread().interrupt();
                 }
+
             }
             System.out.println("Port closed: " + serialPort.closePort());
         } catch (SerialPortException ex) {
             System.out.println(ex);
         }
     }
+
+    private static class SerialPortReader implements SerialPortEventListener {
+
+        public void serialEvent(SerialPortEvent event) {
+            if (event.isCTS()) {//If CTS line has changed state
+                if (event.getEventValue() == 1) {//If line is ON
+                    System.out.println("CTS - ON");
+                } else {
+                    System.out.println("CTS - OFF");
+                }
+            } else if (event.isDSR()) {///If DSR line has changed state
+                if (event.getEventValue() == 1) {//If line is ON
+                    System.out.println("DSR - ON");
+                } else {
+                    System.out.println("DSR - OFF");
+                }
+            } else if (event.isRXCHAR()) {
+                if (event.getEventValue() == 9)
+                    try {
+                        byte buffer[] = serialPort.readBytes(event.getEventValue());
+                        System.out.println("Buffer=" + new String(buffer));
+                    } catch (SerialPortException ex) {
+                        System.out.println(ex);
+                    }
+            }
+        }
+    }
+
 
 }
